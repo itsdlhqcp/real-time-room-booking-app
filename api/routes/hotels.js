bd4e2004faf2b48c58b1f4801cf7c491617b805a
DILHAQUE <95963252+itsdlhqcp@users.noreply.github.com>
2024-07-08 00:23:56 +0530

    first commit

---
+++ api/routes/hotels.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { countByCity, createHotel, deleteHotel, getAllHotels, getHotel, updateHotel } from "../controllers/hotel.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
+
+const router = express.Router();
+
+//CREATE
+router.post("/", verifyAdmin, createHotel);
+//UPDATE
+router.put("/:id", verifyAdmin, updateHotel);
+//DELETE
+router.delete("/:id", verifyAdmin, deleteHotel);
+//GET
+router.get("/find/:id", getHotel);
+//GET ALL
+router.get("/", getAllHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", getAllHotels);
+
+export default router
---
+++ api/routes/users.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { createUser, deleteUser, getAllUsers, getUser, updateUser } from "../controllers/user.js";
+import { verifyUser, verifyAdmin } from "../utils/verifyToken.js";
+
+const router = express.Router();
+
+//CREATE
+// router.post("/", createUser);
+//UPDATE
+router.put("/:id", verifyUser,  updateUser);
+//DELETE
+router.delete("/:id",verifyUser, deleteUser);
+//GET
+router.get("/:id",verifyUser, getUser);
+//GET ALL
+router.get("/",verifyAdmin, getAllUsers);
+
+export default router
