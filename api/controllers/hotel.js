b5635dad963ec817c9f290a02295954da3d0e6ab
DILHAQUE <95963252+itsdlhqcp@users.noreply.github.com>
2024-07-27 00:47:38 +0530

    room reserve pop up functionality upto done

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -1,5 +1,6 @@
 
 import Hotel from "../models/Hotel.js"
+import Room from "../models/Room.js"
 export const createHotel = async (req,res,next)=>{
     const newHotel = new Hotel(req.body);
     try{
@@ -40,7 +41,7 @@ export const getHotel = async (req,res,next)=>{
 // export const getAllHotels = async (req,res,next)=>{
 //     const {min, max, ...others} = req.query;
 //     try{
-//         const getAllHotels = await Hotel.find({...others, cheapestPrice: {$gt: min | 1, $lt: max || 999},}).limit(req.query.limit);
+//         const getAllHotels = await Hotel.find({...others, cheapestPrice: {$gt: min | 1, $lt: max || 999},}).limit(Number(limit) || 0);
 //         res.status(200).json(getAllHotels);
 //        }catch(err){
 //            next(err);
@@ -108,3 +109,17 @@ export const countByType = async (req, res, next) => {
     }
   };
 
+
+  export const getHotelRooms = async (req, res, next) => {
+    try {
+      const hotel = await Hotel.findById(req.params.id);
+      const list = await Promise.all(
+        hotel.rooms.map((room) => {
+          return Room.findById(room);
+        })
+      );
+      res.status(200).json(list)
+    } catch (err) {
+      next(err);
+    }
+  };
