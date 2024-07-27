95530164da84c400780b7bdcd6bf649264ada79a
DILHAQUE <95963252+itsdlhqcp@users.noreply.github.com>
2024-07-27 18:48:56 +0530

    added reservation of room feature

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createRoom, deleteRoom, getRoom, getRooms, updateRoom } from "../controllers/room.js";
+import { createRoom, deleteRoom, getRoom, getRooms, updateRoom, updateRoomAvailability } from "../controllers/room.js";
 import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
@@ -8,10 +8,13 @@ const router = express.Router();
 router.post("/:hotelid", verifyAdmin, createRoom);
 //UPDATE
 router.put("/:id", verifyAdmin, updateRoom);
+//ROOM AVAILABILITY UPDATE 
+router.put("/availability/:id", updateRoomAvailability);
 //DELETE
 router.delete("/:id/:hotelid", verifyAdmin , deleteRoom);
 //GET
 router.get("/:id", getRoom);
+
 //GET ALL
 router.get("/", getRooms);
 
