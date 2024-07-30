226906b4a5102739e965e515db00f34bf1aa604c
DILHAQUE <95963252+itsdlhqcp@users.noreply.github.com>
2024-07-30 18:49:31 +0530

    adding new room feature at admin side added

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -5,7 +5,7 @@ import { verifyAdmin } from "../utils/verifyToken.js";
 const router = express.Router();
 
 //CREATE
-router.post("/:hotelid", verifyAdmin, createRoom);
+router.post("/:hotelid", createRoom);
 //UPDATE
 router.put("/:id", verifyAdmin, updateRoom);
 //ROOM AVAILABILITY UPDATE 
